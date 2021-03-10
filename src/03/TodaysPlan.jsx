e1e0e0ff6e2be845b498a282bebd50ed33e5ed69
asd0905 <asd0905@naver.com>
2021-03-10 18:21:58 +0900

    feat: 03-3 컴포넌트에 데이터를 전달하는 프로퍼티

---
+++ src/03/TodaysPlan.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TodaysPlan extends React.Component {
+    render() {
+        const name = this.props.name;
+        return (
+            <div className="message-container">
+                {name}
+            </div>
+        )
+    }
+}
+
+export default TodaysPlan
--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React, {Component} from "react";
+import React from "react";
+import ChildComponent2 from "./03/ChildComponent2";
+import DefaultPropsComponent from "./03/DefaultPropsComponent";
+import ChildProperty from "./03/ChildProperty";
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-        // 아래는 jsx 입니다.
-        <div>
-            <img src="http://www.easyspub.co.kr/images/logo_footer.png" alt="alt"/>
-            <div>안녕하세요</div>
-        </div>
-    )
-  }
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <ChildProperty>
+                    <div><span>자식노드</span></div>
+                </ChildProperty>
+            </div>
+            /*<div>
+                <DefaultPropsComponent />
+            </div>*/
+            /*<ChildComponent2
+                objValue={{age: 20}}
+                requiredStringValue="문자"
+            />*/
+            /*<div className="body">
+                <ChildComponent
+                booleanValue={true}
+                numValue={1}
+                arrayValue={[1, 2, 3]}
+                objValue={{name: 'aaa', age: 30}}
+                nodeValue={<h1>node</h1>}
+                funcValue={() => {console.log('messea')}}/>
+            </div>*/
+        )
+    }
+
 }
 
 export default App;

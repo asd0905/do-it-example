203c1f785b7a3347b40cccb1c21fcd53e7449579
asd0905 <asd0905@naver.com>
2021-03-11 02:36:18 +0900

    feat: 03-5 컴포넌트의 생명주기

--- src/App.js
+++ src/App.js
@@ -2,16 +2,44 @@ import React from "react";
 import ChildComponent2 from "./03/ChildComponent2";
 import DefaultPropsComponent from "./03/DefaultPropsComponent";
 import ChildProperty from "./03/ChildProperty";
+import ForceUpdateExample from './03/ForceUpdateExample';
+import LifecyleExample from './03/LifecycleExample'
 import './App.css';
+import NewCounter from "./03/NewCounter";
+import Counter from './03/Counter';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { count: 10 };
+        this.resetCount = this.resetCount.bind(this);
+        // this.state = { hasDestroyed: false };
+    }
+    resetCount() {
+        this.setState(({ count }) => ({ count: count + 10 }))
+        console.log(this.state.count);
+    }
+    componentDidMount() {
+        // this.setState({ hasDestroyed: true })
+    }
     render() {
         return (
             <div>
+                <div><Counter count={ this.state.count } /></div>
+                <div><NewCounter count={ this.state.count } /></div>
+                <button onClick={ this.resetCount }>{ this.state.count + 10 }으로 초기화</button>
+            </div>
+            /*<div>
+                {this.state.hasDestroyed? null : <LifecyleExample />}
+            </div>*/
+            // <div>
+            //     <ForceUpdateExample />
+            // </div>
+            /*<div>
                 <ChildProperty>
                     <div><span>자식노드</span></div>
                 </ChildProperty>
-            </div>
+            </div>*/
             /*<div>
                 <DefaultPropsComponent />
             </div>*/
